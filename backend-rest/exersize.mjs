305e9151ad4d6eb4477f534d77f3221208e5db89
LaurenMBell <bell.laurenmar@gmail.com>
2026-05-30 17:13:31 -0700

    first commit (again)

---
+++ backend-rest/exersize.mjs
@@ -0,0 +1,22 @@
+import { uid } from 'uid';
+
+export default class Exersize {
+    /**
+     * 
+     * @param {string} name
+     * @param {Number} reps
+     * @param {Number} weight
+     * @param {String} unit
+     * @param {Date} date
+     * @param {Object} _id
+     */
+
+    constructor(name, reps, weight, unit, date, _id) {
+        this.name = name;
+        this.reps = reps;
+        this.weight = weight;
+        this.unit = unit;
+        this.date = date;
+        this._id = _id;
+    }
+}
