dea97f8b9c7f333b7034417e74ea6950455c5555
LaurenMBell <bell.laurenmar@gmail.com>
2026-05-31 10:57:11 -0700

    troubleshooting mongo db connection and more export issues

--- frontend-react/src/pages/HomePage.jsx
+++ frontend-react/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import ExerciseCollection from '../components/RenameCollection';
+import ExerciseCollection from '../components/ExerciseCollection';
 import { useState} from 'react';
 
 function HomePage() {
