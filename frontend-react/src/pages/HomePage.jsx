2f23e923f15425635c63356483b355cb6c479f1b
LaurenMBell <bell.laurenmar@gmail.com>
2026-06-09 13:37:10 -0700

    added useEffect book

--- frontend-react/src/pages/HomePage.jsx
+++ frontend-react/src/pages/HomePage.jsx
@@ -1,15 +1,26 @@
-import { Link } from 'react-router-dom';
 import ExerciseCollection from '../components/ExerciseCollection';
-import { useState} from 'react';
+import { useState, useEffect } from 'react';
 
 function HomePage() {
     const [Exercises, setExercises] = useState([]);
+
+    useEffect(() => {
+        const fetchExercises = async () => {
+            try {
+                const response = await fetch('/exercises');
+                const data = await response.json();
+                setExercises(data);
+            } catch (error) {
+                console.error('Error fetching exercises:', error);
+            }
+        };
+        fetchExercises();
+    }, []);
 
     return (
         <>
             <h2>List of Exercises</h2>
             <ExerciseCollection Exercises={Exercises}></ExerciseCollection>
-            <Link to="/add-exercise">Add a Exercise</Link>
         </>
     );
 }
